acf0d5ee6e5a50d925350ea21a97a689ec343887
parzivalClaus <claudio@kaizenfotoprodutos.com.br>
2021-04-28 13:39:26 -0300

    Report Generate

---
+++ lib/bookings/report.ex
@@ -0,0 +1,25 @@
+defmodule Flightex.Bookings.Report do
+  alias Flightex.Bookings.Agent, as: BookingAgent
+  alias Flightex.Bookings.Booking
+
+  def create(filename \\ "report.csv") do
+    booking_list = build_booking_list()
+
+    File.write(filename, booking_list)
+  end
+
+  defp build_booking_list() do
+    BookingAgent.list_all()
+    |> Map.values()
+    |> Enum.map(fn booking -> booking_string(booking) end)
+  end
+
+  defp booking_string(%Booking{
+         id_usuario: id_usuario,
+         cidade_origem: cidade_origem,
+         cidade_destino: cidade_destino,
+         data_completa: data_completa
+       }) do
+    "#{id_usuario},#{cidade_origem},#{cidade_destino},#{data_completa}\n"
+  end
+end
